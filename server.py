188fa70f80143a4756e674205555eff94b562acc
metflex <106245122+metflekx@users.noreply.github.com>
2024-10-20 03:53:04 +0300

    Spiral

--- server.py
+++ server.py
@@ -17,5 +17,13 @@ def projects():
 def snake():
     return render_template('snake.html', animation="matrix")
 
+@app.route("/universe")
+def universe():
+    return render_template('universe.html', animation="halloween")
+
+@app.route("/spiral")
+def spiral():
+    return render_template("spiral.html", animation="spiral")
+
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=8080)
